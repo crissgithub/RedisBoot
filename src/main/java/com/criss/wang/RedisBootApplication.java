5d1a40b5cabf793ba7ff767b8daf755524e6b2ab
wangqiubao <wangqb1129@163.com>
2018-10-13 23:59:00 +0800

    c xs

---
+++ src/main/java/com/criss/wang/RedisBootApplication.java
@@ -0,0 +1,12 @@
+package com.criss.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisBootApplication.class, args);
+	}
+}
